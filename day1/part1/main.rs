4eac39260b2ae241316a3b3a8c13bda7b41c4f97
Kristiyan Nikolov <kristiyan.nikolov@equitymultiple.com>
2023-12-05 22:47:04 +0200

    use the builtin is_digit func

--- day1/part1/main.rs
+++ day1/part1/main.rs
@@ -1,21 +1,5 @@
 use std::fs;
 
-fn is_digit(c: &str) -> bool {
-    match c {
-        "0" => true,
-        "1" => true,
-        "2" => true,
-        "3" => true,
-        "4" => true,
-        "5" => true,
-        "6" => true,
-        "7" => true,
-        "8" => true,
-        "9" => true,
-        _ => false,
-    }
-}
-
 fn main() {
     let contents = fs::read_to_string("./input.txt").expect("Something went wrong reading the file");
     let lines = contents.lines();
@@ -24,7 +8,7 @@ fn main() {
     for line in lines {
         let mut line_digits = Vec::new();
         line.chars().for_each(|c| {
-            if is_digit(&c.to_string()) {
+            if c.is_digit(10) {
                 line_digits.push(c);
             }
         });
