2f07a00538d6d557e3a9d206d6d3415e26fdc6e7
Kristiyan Nikolov <kristiyan.nikolov@equitymultiple.com>
2023-12-06 19:04:46 +0200

    a bit simpler

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -21,9 +21,8 @@ fn main() {
     let content = fs::read_to_string("./input.txt")
         .expect("Failed to read file");
     content.lines().for_each(|line| {
-        println!("{}", line);
         let game = line_to_game(line);
-        sum_min += sum_rolls_per_game(&game);
+        sum_min += power_rolls_per_game(&game);
         if game_possible(&game) {
             sum += game.id;
         }
@@ -50,10 +49,13 @@ fn parse_rolls(line: &str) -> Vec<Roll> {
     let mut rolls = Vec::new();
     let rolls_str = line.split(":").collect::<Vec<&str>>()[1];
     rolls_str.split(";").for_each(|roll| {
+
         roll.split(",").for_each(|ball| {
+
             let pair = ball.trim().split(" ").collect::<Vec<&str>>();
             let count = pair[0].trim().parse::<i32>().unwrap();
             let color = pair[1];
+
             match color {
                 "red" => rolls.push(Roll { red: count, green: 0, blue: 0 }),
                 "green" => rolls.push(Roll { red: 0, green: count, blue: 0 }),
@@ -75,7 +77,7 @@ fn parse_game_id(line: &str) -> i32 {
         .unwrap()
 }
 
-fn sum_rolls_per_game(game: &Game) -> i32 {
+fn power_rolls_per_game(game: &Game) -> i32 {
     let mut reds = Vec::new();
     let mut greens = Vec::new();
     let mut blues = Vec::new();
@@ -84,21 +86,9 @@ fn sum_rolls_per_game(game: &Game) -> i32 {
         blues.push(roll.blue);
         greens.push(roll.green);
     }
-    let max_red = max_number(&reds).unwrap();
-    let max_green = max_number(&greens).unwrap();
-    let max_blue = max_number(&blues).unwrap();
 
-    println!("{}", max_red * max_green * max_blue);
-    max_red * max_green * max_blue
-}
-
-fn max_number(numbers: &[i32]) -> Option<i32> {
-    let mut large = numbers.get(0)?;
-    for number in numbers {
-        if large < number {
-            large = number
-        }
-    }
-
-    Some(*large)
+    reds.sort();
+    greens.sort();
+    blues.sort();
+    reds.last().unwrap() * greens.last().unwrap() * blues.last().unwrap()
 }
